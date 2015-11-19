09d5e5ef13538996b22e5f0604543e7aeac53789
Froblinkin <jonathan.perrie@gmail.com>
2015-11-18 22:35:32 -0400

    renaming certain directories and adding some simple obstacles
    The moving platform has some errors with the whentriggerexit method when
    the platform moves vertically, need to fix that asap

---
+++ Assets/Scripts/PlayerDeathBySpikes.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerDeathBySpikes : MonoBehaviour {
+    //If the player comes into contact with the spikes, he/she will automatically die
+    //we can change the effect later if this is too harsh to just losing dmg
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Application.LoadLevel("Demo Scene");
+        }
+    }
+}
