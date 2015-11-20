f42b5830d0f4934ad3f96b0cc7208759b7dbdef8
Al Brink <AlBrink@Als-MacBook-Air.local>
2015-11-20 19:49:20 -0400

    Added Projectiles, character attributes, and health + associated scripts

---
+++ Assets/Scripts/characterStats.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class characterStats : MonoBehaviour {
+
+	public float health = 10;
+	public float speed = 0.25f;
+	public float damage = 1;
+	public float detection = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	public void applyDamage(float damage)
+	{
+		health -= damage;
+		if (health <= 0) {
+			if(this.name == "Player")
+			{
+				//playerDeath
+			}
+			else
+			{
+				Destroy(gameObject);
+			}
+		}
+	}
+	void OnControllerColliderHit (ControllerColliderHit col)
+	{
+		//Debug.Log("PlayerCollision detected");
+
+		if(col.gameObject.tag == "Mummy")
+		{
+			col.gameObject.GetComponent<characterStats>().applyDamage(damage);
+			Rigidbody rb = col.gameObject.GetComponent<BoxCollider>().attachedRigidbody;
+			this.gameObject.GetComponent<CharacterController>().attachedRigidbody.velocity -=  new Vector3(5, 0, 0);
+			Debug.Log("Player Health:" + col.gameObject.GetComponent<characterStats>().health);
+
+		}
+
+
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/projectileAttributes.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class projectileAttributes : MonoBehaviour {
+	public float speed;
+	//Rigidbody rb = GameObject.GetComponent<Rigidbody>();
+	public float damage;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter (Collision col)
+	{
+
+		if(col.gameObject.GetComponent<characterStats>())
+		{
+			col.gameObject.GetComponent<characterStats>().applyDamage(damage);
+			Debug.Log("Target Health:" + col.gameObject.GetComponent<characterStats>().health);
+			Destroy (gameObject);
+		}
+		Destroy (gameObject);
+	}
+}
