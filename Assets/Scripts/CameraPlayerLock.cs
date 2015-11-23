62a2eae24c60f69570bbb7567fa87df2b8300d84
noved210 <devonrimmington@gmail.com>
2015-11-23 14:13:29 -0400

    Player Direction, player tools
    Player will change direction that they are facing. Camera is locked in
    the Z/Y-axis and the player can swap equipment with "UP and DOWN" arrows
    (meshes aren't made yet)

---
+++ Assets/Scripts/CameraPlayerLock.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraPlayerLock : MonoBehaviour {
+
+	public GameObject player;
+	private Vector3 thisPosition;
+
+	// Use this for initialization
+	void Start () {
+		thisPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		gameObject.transform.position = new Vector3 (player.transform.position.x, thisPosition.y, thisPosition.z);
+
+	}
+}
---
+++ Assets/Scripts/EquiptmentSwap.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EquiptmentSwap : MonoBehaviour {
+
+	public GameObject[] equipment;
+	private int currentSelection, selectionMax;
+
+	private Mesh equipmentMesh;
+
+	// Use this for initialization
+	void Start () {
+		currentSelection = 0;
+		selectionMax = equipment.Length;
+		gameObject.AddComponent<MeshFilter> ();
+		gameObject.AddComponent<MeshRenderer> ();
+		equipmentMesh = equipment[currentSelection].GetComponent<MeshFilter> ().sharedMesh;
+		gameObject.GetComponent<MeshFilter> ().mesh = equipmentMesh;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			currentSelection++;
+			if(currentSelection >= selectionMax){
+				currentSelection = 0;
+			}
+			equipmentMesh = equipment[currentSelection].GetComponent<MeshFilter> ().sharedMesh;
+			gameObject.GetComponent<MeshFilter> ().mesh = equipmentMesh;
+		}
+
+		else if (Input.GetKeyDown (KeyCode.DownArrow)) {
+			currentSelection--;
+			if(currentSelection < 0){
+				currentSelection = selectionMax;
+			}
+			equipmentMesh = equipment[currentSelection].GetComponent<MeshFilter> ().sharedMesh;
+			gameObject.GetComponent<MeshFilter> ().mesh = equipmentMesh;
+		}
+
+	}
+}
