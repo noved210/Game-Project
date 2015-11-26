5c27ca526089b910bab938d9e5374ddcf25dca85
Al Brink <AlBrink@Als-MacBook-Air.local>
2015-11-26 19:41:39 -0400

    Added gun aiming; Left & Right bracket keys used

---
+++ Assets/aimEquipment.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class aimEquipment : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.LeftBracket)) {
+			Debug.Log ("Aiming up...");
+			transform.Rotate(-Vector3.forward);
+		}
+		else if (Input.GetKey (KeyCode.RightBracket)) {
+			Debug.Log ("Aiming down...");
+			transform.Rotate(Vector3.forward);
+		}
+	}
+}
