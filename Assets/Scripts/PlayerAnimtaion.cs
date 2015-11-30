c71251a9172296055df48eab7a18144c15ef36aa
Froblinkin <jonathan.perrie@gmail.com>
2015-11-30 14:13:25 -0400

    Submitting Devon's changes + mine
    hopefully this works

---
+++ Assets/Scripts/PlayerAnimtaion.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAnimtaion : MonoBehaviour {
+
+	private Animation animation;
+	private Vector3 lastPosition;
+
+
+	// Use this for initialization
+	void Start () {
+		animation = GetComponent<Animation> ();
+		//animation ["PlayerWalking"].speed = 1;
+		//animation ["PlayerRunning"].speed = 2;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if ((Input.GetKey (KeyCode.D) || Input.GetKey (KeyCode.A)) && !Input.GetKey(KeyCode.LeftShift)) {
+			animation.Play ("PlayerWalking");
+			//animation.clip.frameRate = 30;
+			//lastPosition = transform.position;
+		}else if((Input.GetKey (KeyCode.D) || Input.GetKey (KeyCode.A))&& Input.GetKey(KeyCode.LeftShift)){
+			animation.Play ("PlayerRunning");
+			animation["PlayerRunning"].speed = 2;
+			//animation.clip.frameRate = 120;
+		} else {
+			//if(!animation.IsPlaying("idle")){
+				animation.Play("idle");
+				//transform.position = lastPosition;
+			//}
+		}
+	}
+}
---
+++ Assets/Scripts/SlideFailed.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SlideFailed : MonoBehaviour {
+
+	CharacterController player;
+
+	// Use this for initialization
+	void Start () {
+		player = GetComponent<CharacterController> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	//want only if the player gets hit from above
+	void OnControllerColliderHit(ControllerColliderHit obj){
+		if (obj.gameObject.tag == "SlideSpot") {
+			GameObject[] objs = GameObject.FindGameObjectsWithTag("Respawn");
+			transform.position = getClosestPoint(objs).transform.position;
+		}
+	}
+
+	//Get the point to the NCP
+	GameObject getClosestPoint(GameObject[] objs){
+		
+		float distance = Vector3.Distance (gameObject.transform.position, objs [0].transform.position);
+		int index = 0;
+		
+		for (int i = 1; i < objs.Length; i++) {
+			if (distance > Vector3.Distance (gameObject.transform.position, objs [i].transform.position)) {
+				index = i;
+				distance = Vector3.Distance (gameObject.transform.position, objs [i].transform.position);
+			}
+		}
+
+		return objs [index];
+	}
+		
+
+}
